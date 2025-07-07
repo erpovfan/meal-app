127c477e7cc8da99faf70d175351b39d96501cc0
Erkralfan <161698582+Erkralfan@users.noreply.github.com>
2025-07-07 12:30:55 +0330

    reset

---
+++ mobile/app/test/erfan.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const erfan = () => {
+  return (
+    <View>
+      <Text>erfan</Text>
+    </View>
+  )
+}
+
+export default erfan
